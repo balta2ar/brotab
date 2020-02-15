55b17044178594f8d251723fd285d1c17c68fa2b
Yuri Bochkarev <baltazar.bz@gmail.com>
2020-02-15 15:44:50 +0100

    add maybe_remove_file, fix tests

--- brotab/tests/test_inout.py
+++ brotab/tests/test_inout.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -10,22 +11,24 @@ class TestEditor(TestCase):
     @patch('os.environ', new={})
     def test_run_editor_linux(self, _system_mock, _run_editor_mock):
         assert ['1'] == edit_tabs_in_editor(['1'])
-        editor, _filename = _run_editor_mock.call_args[0]
+        editor, filename = _run_editor_mock.call_args[0]
         assert editor == 'nvim'
+        assert not os.path.exists(filename)
 
     @patch('brotab.inout.run_editor')
     @patch('platform.system', side_effect=['Windows'])
     @patch('os.environ', new={})
     def test_run_editor_windows(self, _system_mock, _run_editor_mock):
         assert ['1'] == edit_tabs_in_editor(['1'])
-        editor, _filename = _run_editor_mock.call_args[0]
+        editor, filename = _run_editor_mock.call_args[0]
         assert editor == 'notepad'
+        assert not os.path.exists(filename)
 
     @patch('brotab.inout.run_editor')
     @patch('platform.system', side_effect=['Windows'])
     @patch('os.environ', new={'EDITOR': 'custom'})
     def test_run_editor_windows_custom(self, _system_mock, _run_editor_mock):
         assert ['1'] == edit_tabs_in_editor(['1'])
-        editor, _filename = _run_editor_mock.call_args[0]
+        editor, filename = _run_editor_mock.call_args[0]
         assert editor == 'custom'
-
+        assert not os.path.exists(filename)
