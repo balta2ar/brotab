4252a2c6118f80435832462e336f15b94fce0717
Yuri Bochkarev <baltazar.bz@gmail.com>
2025-01-11 21:17:31 +0100

    the world has moved on

--- setup.py
+++ setup.py
@@ -152,10 +152,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython'
     ],
     # $ setup.py publish support.
